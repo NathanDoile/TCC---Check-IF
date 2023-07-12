267a0aa14c3641dbca9529c523d220ea1f5f1b1a
NathanDoile <doile.souza@gmail.com>
2023-07-12 15:58:40 -0300

    Adicionando base inicial do projeto

---
+++ api/check_if/src/main/java/br/edu/ifsul/sapucaia/check_if/CheckIfApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.sapucaia.check_if;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckIfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckIfApplication.class, args);
+	}
+
+}
---
+++ api/check_if/src/test/java/br/edu/ifsul/sapucaia/check_if/CheckIfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsul.sapucaia.check_if;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckIfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
